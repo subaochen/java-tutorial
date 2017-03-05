72d87b3c5df4bbe807bb2cbf1031da190e231c8a
JamesLiAndroid <lsy1234567@163.com>
2017-03-06 07:48:40 +0800

    添加Interface实现Enum的代码

---
+++ guide/code/enum/src/cn/edu/sdut/softlab/interfacenum/EnumInterface.java
@@ -0,0 +1,10 @@
+package cn.edu.sdut.softlab.interfacenum;
+
+/**
+ * Created by lisongyang on 17-3-6.
+ */
+public interface EnumInterface {
+    public static final String ONE = "1";
+    public static final int TWO = 2;
+    public static final float THREE = 3.0f;
+}
