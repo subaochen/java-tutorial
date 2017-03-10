161a2fbb86505bd5022211834b81d5d0ae2458b0
subaochen <subaochen@126.com>
2017-03-10 08:36:52 +0800

    暂时不启用提高篇的实验部分，还很不完善

--- advance/makebook.sh
+++ advance/makebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-lyx --export pdf4 java-advanced.lyx
-pdftk cover.pdf java-advanced.pdf cat output ../java-advanced.pdf
-rm java-advanced.pdf
+lyx --export pdf4 java-advanced-tutorial.lyx
+pdftk cover.pdf java-advanced-tutorial.pdf cat output ../java-advanced.pdf
+rm java-advanced-tutorial.pdf
