25e3c13c9ca24ad4250353825830b10aea473e5c
subaochen <subaochen@126.com>
2017-03-19 18:12:23 +0800

    附录增加“常见面试题”部分

---
+++ guide/code/appendix/src/cn/edu/sdut/softlab/interview/StaticDispatch.java
@@ -0,0 +1,31 @@
+package cn.edu.sdut.softlab.interview;
+
+/**
+ * Created by subaochen on 17-3-19.
+ */
+public class StaticDispatch {
+	static abstract class Human {}
+	static class Man extends Human {}
+	static class Woman extends Human {}
+
+	public void sayHello(Human guy) {
+		System.out.println("hello, guy!");
+	}
+
+	public void sayHello(Man guy) {
+		System.out.println("hello, man!");
+	}
+
+	public void sayHello(Woman guy) {
+		System.out.println("hello, woman!");
+	}
+
+	public static void main(String[] args) {
+		Human man = new Man();
+		Human woman = new Woman();
+		StaticDispatch sd = new StaticDispatch();
+		sd.sayHello(man);
+		sd.sayHello(woman);
+		sd.sayHello((Man)man);
+	}
+}
