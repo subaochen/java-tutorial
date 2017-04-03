5f2b40c4242817bf7f89ee00327a95d4fc066b38
subaochen <subaochen@126.com>
2017-04-03 12:34:53 +0800

    编写注解部分，not finished

---
+++ advance/code/annotation/src/cn/edu/sdut/softlab/override/Child.java
@@ -0,0 +1,9 @@
+package cn.edu.sdut.softlab.override;
+
+/**
+ * Created by subaochen on 17-4-3.
+ */
+public class Child extends Parent {
+    @Override
+    public void methodA() {}
+}
---
+++ advance/code/annotation/src/cn/edu/sdut/softlab/override/Parent.java
@@ -0,0 +1,8 @@
+package cn.edu.sdut.softlab.override;
+
+/**
+ * Created by subaochen on 17-4-3.
+ */
+public class Parent {
+    public void method() {}
+}
