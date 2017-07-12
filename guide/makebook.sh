215b33e8d53740c0cfb7158cfd2db973a2aa2a14
su baochen <subaochen@126.com>
2017-07-12 10:10:13 +0800

    自动产生实验指导书

--- guide/makebook.sh
+++ guide/makebook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 lyx --export pdf4 java-guide-on-c.lyx
+lyx --export pdf4 tests-guide.lyx
 pdftk cover.pdf java-guide-on-c.pdf cat output ../java-guide.pdf
-rm java-guide-on-c.pdf
+pdftk cover-test.pdf tests-guide.pdf cat output ../tests-guide.pdf
+rm java-guide-on-c.pdf tests-guide.pdf
