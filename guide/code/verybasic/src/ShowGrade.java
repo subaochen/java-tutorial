84e5b0365dff2e0c8cdaf59ede33ef29e7264470
subaochen <subaochen@125.com>
2017-09-14 11:34:53 +0800

    修改成绩等级的判断方法

--- guide/code/verybasic/src/ShowGrade.java
+++ guide/code/verybasic/src/ShowGrade.java
@@ -12,10 +12,12 @@ public class ShowGrade {
 		System.out.print("请输入成绩："); // |\longremark{在光标闪烁处输入成绩}|
 		float grade = console.nextFloat();
 		System.out.print("成绩" + grade + "的等级为：");
-		if (grade >= 85) {
+		if (grade >= 90) {
 			System.out.println("优秀");
-		} else if (grade >= 75) {
+		} else if (grade >= 80) {
 			System.out.println("良好");
+		} else if (grade >= 70) {
+			System.out.println("中等");
 		} else if (grade >= 60) {
 			System.out.println("及格");
 		} else {
