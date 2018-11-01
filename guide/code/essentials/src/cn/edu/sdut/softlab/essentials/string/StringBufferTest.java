8773298032a8b8993093e86318f234c7516d2d66
subaochen <subaochen@126.com>
2018-11-01 09:51:47 +0800

    StringBufferTest file

---
+++ guide/code/essentials/src/cn/edu/sdut/softlab/essentials/string/StringBufferTest.java
@@ -0,0 +1,17 @@
+package cn.edu.sdut.softlab.essentials.string;
+
+public class StringBufferTest {
+    public static void main(String[] args) {
+        int loops = 20000;
+        System.out.println(System.currentTimeMillis());
+        String s = "";
+        for(int i = 0; i < loops; i++)
+            s = s + i;
+        System.out.println(System.currentTimeMillis());
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < loops; i++)
+            sb.append(i);
+        System.out.println(System.currentTimeMillis());
+    }
+
+}
